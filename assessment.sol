02eaeff67c0584bee228c8586e76454331238206
IshanGthakral <129625994+IshanGthakral@users.noreply.github.com>
2023-07-17 18:29:25 +0530

    Update assessment.sol

--- assessment.sol
+++ assessment.sol
@@ -1,60 +1,78 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event Transfer(address indexed recipient, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
+        uint256 _previousBalance = balance;
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+            revert InsufficientBalance(balance, _withdrawAmount);
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
+        assert(balance == _previousBalance - _withdrawAmount);
         emit Withdraw(_withdrawAmount);
     }
+
+    error InsufficientBalance2(uint256 balance, uint256 _transferAmount);
+
+    function transfer(address payable recipient, uint256 _transferAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint256 _previousBalance = balance;
+        if (balance < _transferAmount) {
+            revert InsufficientBalance2(balance, _transferAmount);
+        }
+
+        balance -= _transferAmount;
+        assert(balance == _previousBalance - _transferAmount);
+        
+        emit Transfer(recipient, _transferAmount);
+    }
+
+    function increaseBalance(uint256 _amount) public payable {
+    require(msg.sender == owner, "You are not the owner of this account");
+    balance += _amount;
+    }
+
+    function decreaseBalance(uint256 _amount) public {
+    require(msg.sender == owner, "You are not the owner of this account");
+    require(balance >= _amount, "Insufficient balance");
+
+    balance -= _amount;
+   }
+
+   function getOwner() public view returns(address) {
+    return owner;
+   }
+
+   function transferOwnership(address payable newOwner) public {
+    require(msg.sender == owner, "You are not the owner of this account");
+    owner = newOwner;
+   }
+
 }
